57e6cdc283a3a6876516c6a3eeb47261aca7f750
EC2 Default User <ec2-user@ip-172-31-29-55.us-east-2.compute.internal>
2019-07-24 14:06:43 +0000

    first commit

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,23 @@ from app.models import model, formopener
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    user = {"name":"Jeffron","favGame":"Debugger"} 
+    return render_template('index.html', user=user)
+
+@app.route('/secret')
+def secret():
+    return render_template('secret.html')
+@app.route('/sush')
+def sush():
+    return "<h1> AHHHHHH!!<h1>"
+@app.route('/sendBreakfast', methods =['GET','POST']) 
+def sendBreakfast():
+    if request.method == 'GET':
+        return "You didnt fill out the form and I bet u say route weirdly. "
+    else:
+        userData = dict(request.form)
+        nickname = userData['nickname'][0]
+        nickname = model.shout(nickname)
+        breakfast = userData['breakfast'][0]
+        breakfast = model.shout(breakfast) 
+        return render_template("breakfast.html", nickname=nickname, breakfast=breakfast)
